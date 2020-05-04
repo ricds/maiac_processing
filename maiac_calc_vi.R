7a6825e8f517d9b7f30889bbe137a538da3f450e
Ricardo Dalagnol <ricds@hotmail.com>
2020-05-04 13:53:58 -0300

    Fixed calc_vi script filenames

--- maiac_calc_vi.R
+++ maiac_calc_vi.R
@@ -44,13 +44,13 @@ f_ndvi = function(NIR, RED) {
 }
 ff_ndvi = cmpfun(f_ndvi)
 
-# Initiate cluster
+# Initiate cluster - Only use parallel if the mosaic is not huge like the whole south america
 #cl = parallel::makeCluster(no_cores)
 #registerDoParallel(cl)
 
-# process (can use parallel if the mosaic is not huge like the whole south america)
+# process
 for (i in 1:dim(composite_vec)[1]) {
-#f=foreach(i = 1:dim(composite_vec)[1], .packages=c("raster"), .errorhandling="remove") %dopar% {
+#f=foreach(i = 1:dim(composite_vec)[1], .packages=c("raster"), .errorhandling="remove") %dopar% { # for parallel
   # check if files exist for given composite
   file_list = list.files(mosaic_output_dir, pattern=as.character(composite_vec[i,]), full.names=TRUE)
   
@@ -68,7 +68,7 @@ for (i in 1:dim(composite_vec)[1]) {
       # apply function
       if (length(grep(file_list, pattern="ndvi", value = T))==0) {
         writeRaster(x=overlay(maiac_band2, maiac_band1, fun=ff_ndvi),
-                    filename = paste0(dirname(grep(file_list, pattern="band1", value=T)), "/", mosaic_base_filename, "_ndvi_", composite_vec[i,], ".tif"),
+                    filename = paste0(dirname(grep(file_list, pattern="band1", value=T)), "/", mosaic_base_filename, "_", composite_vec[i,], "_ndvi_latlon.tif"),
                     overwrite = TRUE, format = "GTiff",
                     datatype = "INT2S",
                     options = c("COMPRESS=LZW","PREDICTOR=2"))
@@ -76,7 +76,7 @@ for (i in 1:dim(composite_vec)[1]) {
       
       if (length(grep(file_list, pattern="evi", value = T))==0) {
         writeRaster(x=overlay(maiac_band2, maiac_band1, maiac_band3, fun=ff_evi),
-                    filename = paste0(dirname(grep(file_list, pattern="band1", value=T)), "/", mosaic_base_filename, "_evi_", composite_vec[i,], ".tif"),
+                    filename = paste0(dirname(grep(file_list, pattern="band1", value=T)), "/", mosaic_base_filename, "_", composite_vec[i,], "_evi_latlon.tif"),
                     overwrite = TRUE, format = "GTiff",
                     datatype = "INT2S",
                     options = c("COMPRESS=LZW","PREDICTOR=2"))
@@ -98,5 +98,5 @@ for (i in 1:dim(composite_vec)[1]) {
 print("Processing finished.")
 
 # check file size
-sum(file.info(list.files(mosaic_output_dir, pattern="ndvi", full.names = TRUE))$size)/(1024*1024*1024)  # 3.756398
-sum(file.info(list.files(mosaic_output_dir, pattern="evi", full.names = TRUE))$size)/(1024*1024*1024)  # 3.737279
+sum(file.info(list.files(mosaic_output_dir, pattern="ndvi", full.names = TRUE))$size)/(1024*1024*1024)
+sum(file.info(list.files(mosaic_output_dir, pattern="evi", full.names = TRUE))$size)/(1024*1024*1024)
