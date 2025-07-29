1f9ca9a7dd9283cbf5c680b03f4f0cfbf6c71ec5
Ricardo Dalagnol <ricds@hotmail.com>
2025-07-29 15:53:07 +0000

    added codes for calc vi and anisotropy from s3

---
+++ calc_anisotropy_maiac.R
@@ -0,0 +1,141 @@
+# Script: Calculate MAIAC anisotropy bands ------------------------------------------------------------
+# Author: Ricardo Dal'Agnol da Silva (ricds@hotmail.com)
+# Date Created: 2021-03-08
+# R version 4.0.2 (2020-06-22)
+#
+
+# clean environment
+rm(list = ls()); gc()
+
+# libraries
+library(raster)
+library(rgdal)
+library(rgeos)
+library(gdalUtils)
+library(sp)
+
+# define if EC2 or WS
+machine = "EC2"
+
+
+# library -----------------------------------------------------------------
+
+# get the folder where the script R functions and config file are placed
+functions_dir = paste0(dirname(rstudioapi::getActiveDocumentContext()$path),"/")
+
+# load functions
+source(paste0(functions_dir, "maiac_processing_functions.R"))
+
+# number of cores
+no_cores = 16
+
+
+# config ------------------------------------------------------------------
+
+# directory where the files are located
+input_back = "s3://ctrees-input-data/modis/AnisoVeg_MCD19A1/v061_1km_monthly_expMay25/mosaic/backscat/"
+input_forward = "s3://ctrees-input-data/modis/AnisoVeg_MCD19A1/v061_1km_monthly_expMay25/mosaic/forwardscat/"
+
+# output directory
+output_dir = "s3://ctrees-input-data/modis/AnisoVeg_MCD19A1/v061_1km_monthly_expMay25/mosaic/anisotropy/"
+dir.create(output_dir, showWarnings = F)
+
+# list of bands
+band_names = c("band1","band2","band3","band4","band5","band6","band7","band8","evi","ndvi","gcc")
+
+# list back files
+list_back = s3_list_bucket(input_back)
+
+# list forward files
+list_forward = s3_list_bucket(input_forward)
+
+# get dates
+unique_dates = unique(substr(basename(list_back), 27, 27+6))
+
+# create list of items to run
+expanded_grid = expand.grid(unique_dates, band_names)
+run_df = paste0(expanded_grid$Var1, "_", expanded_grid$Var2)
+length(run_df)
+head(run_df)
+tail(run_df)
+
+# test
+if (FALSE) {
+  no_cores = 16
+  run_df = run_df[1:64]
+  
+  no_cores = 4
+  run_df = run_df[1:8]
+}
+
+# code ---------------------------------------------------------------------
+
+# Initiate cluster
+cl = parallel::makeCluster(no_cores)
+registerDoParallel(cl)
+
+# run in parallel
+i=1
+#for (i in 1:length(band_names)) {
+f=foreach(i = 1:length(run_df), .packages=c("raster","gdalUtils","rgdal"), .errorhandling="remove") %dopar% {
+  
+  if (TRUE) {
+    
+    # create output filename
+    output_fname_s3 = paste0(output_dir, "maiac_month_1000_anisotropy_",run_df[i],"_latlon.tif")
+    
+    # skip if output exists
+    if (S3_file_exists(output_fname_s3)) {
+      print(paste("Output already exists in s3:", i, "-", output_fname_s3))
+      next
+    }
+    
+    # get input filenames
+    list_back_band = grep(run_df[i], list_back, value=T)
+    list_forward_band = grep(run_df[i], list_forward, value=T)
+    
+    # got data?
+    if (length(list_back_band) != 1 | length(list_forward_band) != 1) {
+      print(paste("Missing data for", i))
+      next
+    }
+    
+    # # calc using raster
+    # r_back = raster(s3_to_vsis(list_back_band))
+    # r_forward = raster(s3_to_vsis(list_forward_band))
+    # r_anisotropy = r_back - r_forward
+    # tmp_fname = paste0(tempfile(), ".tif")
+    # writeRaster(r_anisotropy, tmp_fname, overwrite=T, datatype = "INT2S", options = c("COMPRESS=LZW","PREDICTOR=2"))
+    
+    # calc using gdal_calc
+    tmp_fname = paste0(tempfile(), ".tif")
+    gdal_calc_run = paste("gdal_calc.py",
+                          paste0("--calc \" A - B \" "), 
+                          "-A", s3_to_vsis(list_back_band),
+                          "-B", s3_to_vsis(list_forward_band),
+                          "--format GTiff",
+                          "--type Int16",
+                          "--overwrite",
+                          "--co COMPRESS=DEFLATE",
+                          "--quiet",
+                          "--outfile", tmp_fname)
+    system(gdal_calc_run)
+    # file.info(tmp_fname)$size/(1024*1024)
+    
+    # copy file to s3
+    S3_copy_single(tmp_fname, output_fname_s3, "ctrees")
+    
+    # print message
+    #print(paste(i, length(band_names), b_name))
+    #rm(r_back, r_forward, r_anisotropy, b_name, f_name, tmp_fname)
+    print(i)
+    rm(list_back_band, list_forward_band, tmp_fname, output_fname_s3)
+    
+  }
+  
+}
+# 24.5 sec in c5.4xlarge
+
+# finish cluster
+stopCluster(cl)
+
---
+++ maiac_calc_vi_fromS3.R
@@ -0,0 +1,258 @@
+##################################################
+## Project: maiac_processing (https://github.com/ricds/maiac_processing)
+## Script purpose: calculate vegetation indices NDVI and EVI using the maiac composites
+## Author: Ricardo Dal'Agnol da Silva (ricds@hotmail.com)
+## Date: 2017-06-27
+## Notes: get files from "mosaic_output_dir" from "config.txt", calc VI and save them in the same directory
+##################################################
+
+# clean environment
+rm(list = ls())
+
+# load required libraries
+library(raster)  #install.packages("raster")
+library(foreach)  #install.packages("foreach") # click yes if asked
+library(doParallel)  #install.packages("doParallel")
+library(rstudioapi)  #install.packages("rstudioapi")
+library(compiler)  #install.packages("compiler")
+library(itertools)  #install.packages("itertools")
+
+# get the folder where the script R functions and config file are placed
+functions_dir = paste0(dirname(rstudioapi::getActiveDocumentContext()$path),"/")
+
+# load config.txt file that should be in the same directory of the scripts
+machine = "EC2"
+source(paste0(functions_dir, "config_mosaic_vi.txt"))
+
+# load functions
+source(paste0(functions_dir, "maiac_processing_functions.R"))
+
+# read composite vector from the functions folder
+composite_vec = data.frame(createCompDates(composite_no, end_year = end_year))
+
+# function to calc evi
+f_evi = function(NIR, RED, BLUE) {
+  C1 = 6
+  C2 = 7.5
+  L = 1
+  G = 2.5
+  EVI = G*{NIR-RED}/{NIR+{C1*RED}-{C2*BLUE}+L}
+  EVI*10000
+}
+ff_evi = cmpfun(f_evi)
+
+# function to calc ndvi
+f_ndvi = function(NIR, RED) {
+  NDVI = {NIR-RED}/{NIR+RED}
+  NDVI*10000
+}
+ff_ndvi = cmpfun(f_ndvi)
+
+# function to calc gcc
+f_gcc = function(RED, GREEN, BLUE) {
+  GCC = {GREEN}/{RED+GREEN+BLUE}
+  GCC*10000
+}
+ff_gcc = cmpfun(f_gcc)
+
+# input directory
+s3_input_dir = paste0(s3_dir, "mosaic/", view_geometry, "/")
+s3_input_dir_list = s3_list_bucket(s3_input_dir)
+
+# function to apply the 10000 scaling
+scale_data = function(input_fname, output_fname) {
+  gdal_calc_run = paste("gdal_calc.py",
+                        paste0("--calc \" A / 10000 \" "), 
+                        "-A", input_fname,
+                        "--format GTiff",
+                        "--type Float32",
+                        "--overwrite",
+                        "--co COMPRESS=DEFLATE",
+                        "--NoDataValue 32767",
+                        "--quiet",
+                        "--outfile", output_fname)
+  system(gdal_calc_run)
+}
+
+# run ---------------------------------------------------------------------
+
+#
+no_cores = 8
+
+# Initiate cluster - Only use parallel if the mosaic is not huge like the whole south america
+cl = parallel::makeCluster(no_cores)
+registerDoParallel(cl)
+
+# process
+i=1
+#for (i in 1:dim(composite_vec)[1]) {
+f=foreach(i = 1:dim(composite_vec)[1], .packages=c("raster"), .errorhandling="remove") %dopar% { # for parallel
+  
+  if (TRUE) {
+    
+    # check if files exist for given composite
+    file_list = grep(as.character(composite_vec[i,]), s3_input_dir_list, value=T)
+    
+    # if there are files with the given composite name
+    if (length(file_list)!=0) {
+      
+      # define output filenames
+      ndvi_fname = paste0(dirname(grep(file_list, pattern="band1", value=T)), "/", mosaic_base_filename, "_", view_geometry, "_", composite_vec[i,], "_ndvi_latlon.tif")
+      evi_fname = paste0(dirname(grep(file_list, pattern="band1", value=T)), "/", mosaic_base_filename, "_", view_geometry, "_", composite_vec[i,], "_evi_latlon.tif")
+      gcc_fname = paste0(dirname(grep(file_list, pattern="band1", value=T)), "/", mosaic_base_filename, "_", view_geometry, "_", composite_vec[i,], "_gcc_latlon.tif")
+      
+      # skip if output already exists
+      NDVI_EXISTS = S3_file_exists(ndvi_fname) 
+      EVI_EXISTS = S3_file_exists(evi_fname) 
+      GCC_EXISTS = S3_file_exists(gcc_fname) 
+      if (NDVI_EXISTS & EVI_EXISTS & GCC_EXISTS) next
+      
+      # convert s3 to vsis
+      file_list = s3_to_vsis(file_list)
+      
+      # # get bands
+      # maiac_band1 = raster(grep(file_list, pattern="band1", value=T))/10000
+      # maiac_band2 = raster(grep(file_list, pattern="band2", value=T))/10000
+      # maiac_band3 = raster(grep(file_list, pattern="band3", value=T))/10000
+      # maiac_band4 = raster(grep(file_list, pattern="band4", value=T))/10000
+      # 
+      # # apply function
+      # if (!NDVI_EXISTS) {
+      #   ndvi_tmp = paste0(tempfile(), ".tif")
+      #   writeRaster(x=overlay(maiac_band2, maiac_band1, fun=ff_ndvi),
+      #               filename = ndvi_tmp,
+      #               overwrite = TRUE, format = "GTiff",
+      #               datatype = "INT2S",
+      #               options = c("COMPRESS=LZW","PREDICTOR=2"))
+      # }
+      # 
+      # if (!EVI_EXISTS) {
+      #   evi_tmp = paste0(tempfile(), ".tif")
+      #   writeRaster(x=overlay(maiac_band2, maiac_band1, maiac_band3, fun=ff_evi),
+      #               filename = evi_tmp,
+      #               overwrite = TRUE, format = "GTiff",
+      #               datatype = "INT2S",
+      #               options = c("COMPRESS=LZW","PREDICTOR=2"))
+      # }
+      # 
+      # 
+      # if (!GCC_EXISTS) {
+      #   gcc_tmp = paste0(tempfile(), ".tif")
+      #   writeRaster(x=overlay(maiac_band1, maiac_band4, maiac_band3, fun=ff_gcc),
+      #               filename = gcc_tmp,
+      #               overwrite = TRUE, format = "GTiff",
+      #               datatype = "INT2S",
+      #               options = c("COMPRESS=LZW","PREDICTOR=2"))
+      # }
+      
+      # get bands
+      maiac_band1 = grep(file_list, pattern="band1", value=T)
+      maiac_band2 = grep(file_list, pattern="band2", value=T)
+      maiac_band3 = grep(file_list, pattern="band3", value=T)
+      maiac_band4 = grep(file_list, pattern="band4", value=T)
+      
+      # check length
+      if (length(maiac_band1) != 1 & length(maiac_band2) != 1 & length(maiac_band3) != 1 & length(maiac_band4) != 1) next
+      
+      # prepare data
+      maiac_band1_tmp = paste0(tempfile(), ".tif")
+      maiac_band2_tmp = paste0(tempfile(), ".tif")
+      maiac_band3_tmp = paste0(tempfile(), ".tif")
+      maiac_band4_tmp = paste0(tempfile(), ".tif")
+      scale_data(maiac_band1, maiac_band1_tmp)
+      scale_data(maiac_band2, maiac_band2_tmp)
+      scale_data(maiac_band3, maiac_band3_tmp)
+      scale_data(maiac_band4, maiac_band4_tmp)
+      
+      
+      # calc ndvi
+      if (!NDVI_EXISTS) {
+        # NDVI = {NIR-RED}/{NIR+RED}
+        # NDVI*10000
+        ndvi_tmp = paste0(tempfile(), ".tif")
+        gdal_calc_run = paste("gdal_calc.py",
+                              paste0("--calc \" ((A-B)/(A+B))*10000 \" "), 
+                              "-A", maiac_band2_tmp,
+                              "-B", maiac_band1_tmp,
+                              "--format GTiff",
+                              "--type Int16",
+                              "--overwrite",
+                              "--co COMPRESS=DEFLATE",
+                              "--quiet",
+                              "--outfile", ndvi_tmp)
+        system(gdal_calc_run)
+      }
+      
+      # calc ndvi
+      if (!EVI_EXISTS) {
+        # C1 = 6
+        # C2 = 7.5
+        # L = 1
+        # G = 2.5
+        # EVI = G*{NIR-RED}/{NIR+{C1*RED}-{C2*BLUE}+L}
+        # EVI*10000
+        evi_tmp = paste0(tempfile(), ".tif")
+        gdal_calc_run = paste("gdal_calc.py",
+                              paste0("--calc \" (2.5*(A-B)/(A+(6*B)-(7.5*C)+1))*10000 \" "), 
+                              "-A", maiac_band2_tmp, #nir
+                              "-B", maiac_band1_tmp, #red
+                              "-C", maiac_band3_tmp, #blue
+                              "--format GTiff",
+                              "--type Int16",
+                              "--overwrite",
+                              "--co COMPRESS=DEFLATE",
+                              "--quiet",
+                              "--outfile", evi_tmp)
+        system(gdal_calc_run)
+      }
+      
+      # calc ndvi
+      if (!GCC_EXISTS) {
+        # GCC = {GREEN}/{RED+GREEN+BLUE}
+        # GCC*10000
+        gcc_tmp = paste0(tempfile(), ".tif")
+        gdal_calc_run = paste("gdal_calc.py",
+                              paste0("--calc \" (B/(A+B+C))*10000 \" "), 
+                              "-A", maiac_band1_tmp,
+                              "-B", maiac_band4_tmp,
+                              "-C", maiac_band3_tmp,
+                              "--format GTiff",
+                              "--type Int16",
+                              "--overwrite",
+                              "--co COMPRESS=DEFLATE",
+                              "--quiet",
+                              "--outfile", gcc_tmp)
+        system(gdal_calc_run)
+      }
+      
+      # upload results
+      S3_copy_single(ndvi_tmp, ndvi_fname, "ctrees")
+      S3_copy_single(evi_tmp, evi_fname, "ctrees")
+      S3_copy_single(gcc_tmp, gcc_fname, "ctrees")
+      
+      # clean files
+      file.remove(ndvi_tmp, evi_tmp, gcc_tmp, maiac_band1_tmp, maiac_band2_tmp, maiac_band3_tmp, maiac_band4_tmp)
+      
+    }
+    
+    
+    # progress monitor
+    print(paste(i,dim(composite_vec)[1]))
+    
+    # clear
+    gc()
+    removeTmpFiles(h=0)
+    
+  }
+  
+}
+
+# finish cluster
+stopCluster(cl)
+
+# message
+print("Processing finished.")
+
+# check file size
+#sum(file.info(list.files(mosaic_output_dir, pattern="ndvi", full.names = TRUE))$size)/(1024*1024*1024)
+#sum(file.info(list.files(mosaic_output_dir, pattern="evi", full.names = TRUE))$size)/(1024*1024*1024)
